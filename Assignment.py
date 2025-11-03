b9f0ce5942375422997504d575dc72eb69bfa832
Vishal Pandey <vishalpandey87078@gmail.com>
2025-11-03 10:04:24 +0530

    Updated

---
+++ Assignment.py
@@ -0,0 +1,21 @@
+print(f"from, ")
+print("Aareen Dakway")
+print("Sector 36 Kharghar")
+print("Navi Mumbai, Maharashtra")
+print("India - 410210") 
+
+name = input("Enter your name: ")
+print(f"Hello! {name}")
+
+while True:
+    Question = input("Ask me anything: ")
+    if Question.lower() == "stop":
+        print("Bye!")
+        break
+    print("For real Bruh")
+
+while True:
+    alert = input("Submit it already ")
+    if alert.lower() == "done":
+        print("GOOD!")
+        break
